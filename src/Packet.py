5ea85c2e1c4c4afd76c7dca3c66bf455343ab81d
Allarious <alirezaicq4@example.com>
2019-01-16 14:20:10 +0330

    completed Packet.py, we can build packets and parse them from buffers now

--- src/Packet.py
+++ src/Packet.py
@@ -174,21 +174,32 @@
 
                 Root in an answer to the Reunion Hello message will send this packet to the target node.
                 In this packet, all the nodes (IP, port) exist in order by path traversal to target.
-            
+
     
 """
 from struct import *
-
+import struct
 
 class Packet:
-    def __init__(self, buf):
+    def __init__(self, buffer):
         """
         The decoded buffer should convert to a new packet.
 
         :param buf: Input buffer was just decoded.
         :type buf: bytearray
         """
-        pass
+
+        self.buffer = buffer
+
+        buf = struct.unpack("<%dB" % (len(buffer)) ,buffer)
+
+        self.version = buf[:2]
+        self.type = buf[2:4]
+        self.length = buf[4:8]
+        self.server_ip = buf[8:16]
+        self.server_port = buf[16:20]
+        self.header = buf[:20]
+        self.body = buf[20:]
 
     def get_header(self):
         """
@@ -196,7 +207,7 @@ class Packet:
         :return: Packet header
         :rtype: str
         """
-        pass
+        return self.header
 
     def get_version(self):
         """
@@ -204,7 +215,7 @@ class Packet:
         :return: Packet Version
         :rtype: int
         """
-        pass
+        return self.version
 
     def get_type(self):
         """
@@ -212,7 +223,7 @@ class Packet:
         :return: Packet type
         :rtype: int
         """
-        pass
+        return self.type
 
     def get_length(self):
         """
@@ -220,7 +231,7 @@ class Packet:
         :return: Packet length
         :rtype: int
         """
-        pass
+        return self.length
 
     def get_body(self):
         """
@@ -228,7 +239,7 @@ class Packet:
         :return: Packet body
         :rtype: str
         """
-        pass
+        return self.body
 
     def get_buf(self):
         """
@@ -237,7 +248,7 @@ class Packet:
         :return The parsed packet to the network format.
         :rtype: bytearray
         """
-        pass
+        return self.buffer
 
     def get_source_server_ip(self):
         """
@@ -245,7 +256,7 @@ class Packet:
         :return: Server IP address for the sender of the packet.
         :rtype: str
         """
-        pass
+        return self.server_ip
 
     def get_source_server_port(self):
         """
@@ -253,7 +264,7 @@ class Packet:
         :return: Server Port address for the sender of the packet.
         :rtype: str
         """
-        pass
+        return self.server_port
 
     def get_source_server_address(self):
         """
@@ -261,7 +272,7 @@ class Packet:
         :return: Server address; The format is like ('192.168.001.001', '05335').
         :rtype: tuple
         """
-        pass
+        return (self.get_source_server_ip(), self.server_port)
 
 
 class PacketFactory:
@@ -270,7 +281,7 @@ class PacketFactory:
     """
 
     @staticmethod
-    def parse_buffer(buffer):
+    def parse_buffer(buffer) -> Packet:
         """
         In this function we will make a new Packet from input buffer with struct class methods.
 
@@ -280,10 +291,11 @@ class PacketFactory:
         :rtype: Packet
 
         """
-        pass
+
+        return Packet(buffer)
 
     @staticmethod
-    def new_reunion_packet(type, source_address, nodes_array):
+    def new_reunion_packet(type, source_address, nodes_array) -> Packet:
         """
         :param type: Reunion Hello (REQ) or Reunion Hello Back (RES)
         :param source_address: IP/Port address of the packet sender.
@@ -296,10 +308,20 @@ class PacketFactory:
         :return New reunion packet.
         :rtype Packet
         """
-        pass
+        packet_fact = PacketFactory()
+
+        narray = b''
+
+        for x in nodes_array:
+            narray += bytes(str(x[0]), encoding='utf-8')
+            narray += bytes(str(x[1]), encoding='utf-8')
+
+
+        buffer = packet_fact.set_header(1, 5, len(nodes_array)*20 + 5, source_address[0], source_address[1]) + bytes(str(type), encoding='utf-8') + bytes(str(len(nodes_array)), encoding='utf-8') + narray
+        return Packet(buffer)
 
     @staticmethod
-    def new_advertise_packet(type, source_server_address, neighbour=None):
+    def new_advertise_packet(type, source_server_address, neighbour=None) -> Packet:
         """
         :param type: Type of Advertise packet
         :param source_server_address Server address of the packet sender.
@@ -313,10 +335,20 @@ class PacketFactory:
         :rtype Packet
 
         """
-        pass
+        packet_fact = PacketFactory()
+        len = 3
+        neighbour_ip = b''
+
+        if(neighbour):
+            neighbour_ip = bytes(str(neighbour[0]), encoding='utf-8') + bytes(str(neighbour[1]), encoding='utf-8')
+            len = 23
+
+        buffer = packet_fact.set_header(1, 2, len, source_server_address[0], source_server_address[1]) + bytes(str(type), encoding='utf-8') + neighbour_ip
+
+        return Packet(buffer)
 
     @staticmethod
-    def new_join_packet(source_server_address):
+    def new_join_packet(source_server_address) -> Packet:
         """
         :param source_server_address: Server address of the packet sender.
 
@@ -326,10 +358,12 @@ class PacketFactory:
         :rtype Packet
 
         """
-        pass
+        packet_fact = PacketFactory()
+        buffer = packet_fact.set_header(1, 3, 4, source_server_address[0], source_server_address[1]) + b'JOIN'
+        return Packet(buffer)
 
     @staticmethod
-    def new_register_packet(type, source_server_address, address=(None, None)):
+    def new_register_packet(type, source_server_address, address=(None, None)) -> Packet:
         """
         :param type: Type of Register packet
         :param source_server_address: Server address of the packet sender.
@@ -343,10 +377,23 @@ class PacketFactory:
         :rtype Packet
 
         """
-        pass
+        packet_fact = PacketFactory()
+        if(type == 'REQ'):
+            len = 23
+            ip_address = bytes(str(address[0]), encoding='utf-8')
+            port_address = bytes(str(address[1]), encoding='utf-8')
+            bdy = ip_address + port_address
+        else:
+            len = 6
+            ip_address = b''
+            port_address = b''
+            bdy = b'ACK'
+
+        buffer = packet_fact.set_header(1, 1, len, source_server_address[0], source_server_address[1]) + bytes(str(type), encoding='utf-8') + bdy
+        return Packet(buffer)
 
     @staticmethod
-    def new_message_packet(message, source_server_address):
+    def new_message_packet(message, source_server_address) -> Packet:
         """
         Packet for sending a broadcast message to the whole network.
 
@@ -359,4 +406,65 @@ class PacketFactory:
         :return: New Message packet.
         :rtype: Packet
         """
-        pass
+        packet_fact = PacketFactory()
+        buffer = packet_fact.set_header(1, 4, len(message), source_server_address[0], source_server_address[1]) + bytes(str(message), encoding='utf-8')
+        return Packet(buffer)
+
+    @staticmethod
+    def set_header(version, type, length, ip, port) -> Packet:
+        result = b''
+        result += struct.pack("I", version)[1::-1]
+        result += struct.pack("I", type)[1::-1]
+        result += struct.pack("I", length)[::-1]
+        for s in str(ip).split('.'):
+            result += struct.pack("I", int(s))[1::-1]
+
+        result += struct.pack("I", int(port))[::-1]
+        return result
+
+
+
+# print(struct.pack("I", 256)[1::-1])
+
+# print(b'' + bytes('Hello', encoding='utf-8'))
+
+# buf = [0, 1, 0, 3, 0, 0, 255, 10, 0, 20, 0 , 10, 0, 1, 0, 1, 5, 5, 5, 5, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0 ,3]
+#
+# print(buf)
+#
+# pac = struct.pack('<%dB' % len(buf), *buf)
+#
+# print(pac)
+# packet_fact = PacketFactory()
+# pack = packet_fact.parse_buffer(pac)
+# nodes_array = [('192.168.1.1' , '65000'), ('192.168.1.1' , '65000'), ('192.168.1.1', '65000')]
+# pack = packet_fact.new_reunion_packet('REQ', ('192.168.1.1','65000'), nodes_array)
+# pack = packet_fact.new_advertise_packet('RES', ('192.168.1.1','65000'), ('192.168.1.1','65000'))
+# pack = packet_fact.new_message_packet('Hello World!', ('192.168.1.1','65000'))
+# pack = packet_fact.new_register_packet('REQ', ('192.168.1.1','65000'), ('192.168.1.1','65000'))
+#
+#
+# new_pac = struct.unpack("<%dI" % (len(pac) // 4) ,pac)
+
+
+# buf = "{0:b}".format(1).zfill(16) + "{0:b}".format(3).zfill(16) + "{0:b}".format(250).zfill(32) + \
+#         "{0:b}".format(192).zfill(16) + "{0:b}".format(168).zfill(16) + "{0:b}".format(1).zfill(16) + \
+#         "{0:b}".format(1).zfill(16) + "{0:b}".format(5335).zfill(32) + "{0:b}".format(100).zfill(1000)
+
+
+# pack = Packet(buf)
+
+# print("{}".format(buf))
+# print("{}".format(Struct.pack(buf)))
+#
+# print("header: {}".format(pack.get_header()))
+# print("version: {}".format(pack.get_version()))
+# print("type: {}".format(pack.get_type()))
+# print("length: {}".format(pack.get_length()))
+# print("source ip: {}".format(pack.get_source_server_ip()))
+# print("source port: {}".format(pack.get_source_server_port()))
+# print("address: {}".format(pack.get_source_server_address()))
+# print("body: {}".format(pack.get_body()))
+# print("body: {}".format(pack.get_buf()))
+#
+# print(packet_fact.set_header(1, 4, 12, '192.168.001.001', 65000))
