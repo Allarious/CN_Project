ea3e04e39e83fba300ffd7a291efa6954e0391c9
Allarious <alirezaicq4@gmail.com>
2019-01-21 21:31:36 +0330

    Update Peer.py

--- src/Peer.py
+++ src/Peer.py
@@ -7,6 +7,7 @@ import time
 import threading
 from datetime import datetime
 from datetime import timedelta
+import sys
 
 """
     Peer is our main object in this project.
@@ -62,7 +63,6 @@ class Peer:
             self.reunion_mode = None
             self.last_reunion_sent_time = None
         t = threading.Thread(target=self.run_reunion_daemon)
-        t.start()
 
     def start_user_interface(self):
         """
@@ -209,7 +209,15 @@ class Peer:
         :type packet Packet
 
         """
+        
         type = packet.get_type()
+        if packet.get_version() != 1:
+            print("unsupported version", file=sys.stderr)
+            raise ValueError
+        if type < 1 or type > 5:
+            print("unknown packet type", file=sys.stderr)
+            raise ValueError
+            
         if (type == 1):
             self.__handle_register_packet(packet)
         elif (type == 2):
@@ -312,7 +320,7 @@ class Peer:
 
         :return:
         """
-        pass
+        self.stream.broadcast_to_none_registers(packet.get_buf(), packet.get_source_server_address())
 
     def __handle_reunion_packet(self, packet):
         """
